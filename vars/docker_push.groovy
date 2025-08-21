6b5d2baad13185cab5d161c168b411aaccc348dc
Abdul Momin Khan <143100017+Abdul-momin24@users.noreply.github.com>
2025-08-21 20:28:47 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,8 +1,9 @@
-def call(String Project, String ImageTag, String dockerhubuser){
+def call(String Project, String ImageTag, String dockerhubuser) {
     withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
-        sh "echo ${dockerhubpass} | docker login -u ${dockerhubuser} --password-stdin"
+        sh '''
+            echo "$dockerhubpass" | docker login -u "$dockerhubuser" --password-stdin
+            docker images | grep "$Project"
+            docker push "$dockerhubuser/$Project:$ImageTag"
+        '''
     }
-    
-    sh "docker images | grep ${Project}"
-    sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
